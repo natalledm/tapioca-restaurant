c3c82914a191899d699e2735474e28eb4234dc8a
Natalle <natallemoura7@gmail.com>
2022-03-11 00:45:31 +0100

    Navbar done

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import logo from '../../assets/images/tapioca-nav-logo.png';
+import './Navbar.css';
 
 export default function Navbar() {
-  return(
-    <nav>
-      <span>Tapioca</span>
-      <ul>
-        <li><Link to="/dishes">Dishes</Link></li>
-        <li><Link to="/desserts">Desserts</Link></li>
-        <li><Link to="/drinks">Drinks</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+  return (
+    <nav className="navigation-bar">
+      <ul className="nav-links-list">
+        <li><Link to="/"><img src={logo} alt="Tapioca logo" className="nav-logo" /></Link></li>
+        <span className="nav-links-items-container">
+          <li><Link to="/dishes">Dishes</Link></li>
+          <li><Link to="/desserts">Desserts</Link></li>
+          <li><Link to="/drinks">Drinks</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </span>
       </ul>
     </nav>
   )
