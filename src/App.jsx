19e5baad648beb816101221b6ff023bc89544ffb
Natalle <natallemoura7@gmail.com>
2022-03-10 23:53:12 +0100

    CSS reset and root properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/layout/Navbar';
 import Content from './components/layout/Content';
 import Footer from './components/layout/Footer';
+import './App.css';
 
 export default function App() {
   return (
