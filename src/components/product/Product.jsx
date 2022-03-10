2c5ea6769cfd2a644610450bf6e721967e320f89
Natalle <natallemoura7@gmail.com>
2022-03-10 20:25:00 +0100

    Navigation and skeleton data working

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,26 @@
-export default function Product({ item }) {
-  const {} = item;
+import { useParams } from "react-router-dom";
+import products from '../../data/products.json';
+
+export default function Product(props) {
+
+  const params = useParams();
+  const { id } = params;
+
+  const { category } = props;
+
+  const categoryItems = products.filter(item => {
+    return item.category === category;
+  })[0].items;
+
+  const entry = categoryItems.filter(item => {
+    return item.id === +id;
+  })[0];
 
   return (
-    <div></div>
+    <>
+      <h1>{category} {id}</h1>
+      <h2>{JSON.stringify(entry, null, 4)}</h2>
+      <h3>{JSON.stringify(categoryItems, null, 4)}</h3>
+    </>
   );
 };
--- src/components/product/ProductsPage.jsx
+++ src/components/product/ProductsPage.jsx
@@ -1,6 +1,27 @@
+import products from '../../data/products.json';
+import { Link, Outlet } from 'react-router-dom';
+
+
+
 export default function ProductsPage({ category }) {
 
+  const product = products.filter(product => {
+    return product.category === category;
+  })[0]
+
+
+
+
+
   return (
-    <div> { category } Products here</div>
+    <div>
+      {category} {JSON.stringify(product, null, 4)}
+
+      {product.items.map(item => {
+        return <Link to={`/${category}/${item.id}`}>{item.name}</Link>
+      })}
+
+      <Outlet />
+    </div>
   )
 }
