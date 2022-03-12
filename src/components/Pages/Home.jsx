9eb217c825aa86bb35217faea2e127213ba287be
Natalle <natallemoura7@gmail.com>
2022-03-12 14:11:11 +0100

    Hero section done

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,11 +1,10 @@
 import logo from '../../assets/images/tapioca-logo-desktop.png';
-import background from '../../assets/images/bg-pattern.png';
 import './home.css';
 
 export default function Home() {
   return (
     <main className='home'>
-      <section className='hero' style={{ backgroundImage: `url(${background})` }}>
+      <section className='hero'>
         <img src={logo} alt="Tapioca Vegan Restaurant" className='hero-logo' />
       </section>
     </main>
