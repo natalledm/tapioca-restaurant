8e5ced193ef126d3e65c0179e4faf85fbd4d5aa7
Natalle <natallemoura7@gmail.com>
2022-03-12 19:23:37 +0100

    Create products page and connect it to typecard

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,10 +1,22 @@
-export default function ProductCard() {
+import { Link } from 'react-router-dom';
+import './product-card.css';
+
+export default function ProductCard({ item }) {
+  const { photo, name, description, category, id } = item;
+
+  const photoImg = require(`../../assets/photos/${photo}`);
+
   return (
     <div>
-      <p>Image</p>
-      <h2>Product Name</h2>
-      <p>Paragraph about type</p>
-      <button>View type menu</button>
+      <div className="meal-photo-container">
+        <img src={photoImg} alt={name} className="meal-photo" />
+      </div>
+      <div className='meal-content'>
+        <h2 className='meal-title'>{name}</h2>
+        <p className='meal-description'>{description}</p>
+        <a>View {name} details</a>
+        <p className='meal-link'><Link to={`/${category}/${id}`}> &gt; View details of {name}</Link></p>
+      </div>
     </div>
   )
 }
