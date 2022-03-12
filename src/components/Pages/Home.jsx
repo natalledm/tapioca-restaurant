40fcb4a7be8f5cb5181ea59297b577a58e272baa
Natalle <natallemoura7@gmail.com>
2022-03-12 16:03:31 +0100

    product type component added with its css

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,11 +1,21 @@
 import logo from '../../assets/images/tapioca-logo-desktop.png';
+import TypeCard from '../product/TypeCard';
+import products from '../../data/products.json';
 import './home.css';
 
 export default function Home() {
+
+  const productsType = products.map((item, index) => {
+    return <TypeCard key={index} item={item} />
+  })
+  
   return (
     <main className='home'>
       <section className='hero'>
         <img src={logo} alt="Tapioca Vegan Restaurant" className='hero-logo' />
+      </section>
+      <section className='products-type-container'>
+        {productsType}
       </section>
     </main>
   )
---
+++ src/components/product/ProductCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard() {
+  return (
+    <div>
+      <p>Image</p>
+      <h2>Product Name</h2>
+      <p>Paragraph about type</p>
+      <button>View type menu</button>
+    </div>
+  )
+}
